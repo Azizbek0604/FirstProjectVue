4089f0ed0acf26598a46b26a673ff7a2e8605ab3
Azizbek0406 <azizbekcoder04@gmail.com>
2024-07-23 13:39:59 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+    <h1>Counter: {{counter}}</h1>
+    <div>
+    <button @click='onIncrement' class="btn btn-success">Plus</button>
+    <button @click='onDecrement' class="btn btn-danger">Munis</button>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                counter: 0,
+            };
+        },
+    methods: {
+        onIncrement() {
+            this.counter += 1
+        },
+        onDecrement() {
+            this.counter -= 1
+        },
+    },
+    };
+
+</script>
+
+<style>
+</style>
